de3b815b8c94f0e1adfc5ea66c734d91fb19fa55
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-10-11 16:21:06 +0500

    learn elements and components

--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const text = 'Hello World'
-
-const elem = (
-  <div>
-    <h2>{text}</h2>
-    <input type="text" />
-    <button>Click</button>
-  </div>
-)
-
-// const elem = React.createElement('h2', {className: 'title'}, 'Hellow World')
+// import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App />
 );
 
